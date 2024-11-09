98ba6e1899b2905d0c93177f812de077262e5e98
Giovanny <giovanny.tapiero@pi.edu.co>
2024-11-09 10:55:29 -0500

    commit 1

--- consola_octagono/Program.cs
+++ consola_octagono/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int size = 5; // Tamaño del octágono
+        PrintOctagon(size);
+    }
+
+    static void PrintOctagon(int size)
+    {
+        int n = size * 2 - 1;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i < size && j < size - i || i < size && j >= size + i ||
+                    i >= size && j < i - size + 1 || i >= size && j >= n - (i - size + 1))
+                {
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.Write("*");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
